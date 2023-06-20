6b684f03316f9b09089bd2a3fe3f26cd67cda2be
kopyl <oleggggg1997@gmail.com>
2023-06-20 14:21:20 +0300

    Remove test code

--- onnx/flask-server.py
+++ onnx/flask-server.py
@@ -96,11 +96,7 @@ def home_get():
 
 
 if __name__ == "__main__":
-    # app.run(debug=False, host="0.0.0.0", port=80)
+    app.run(debug=False, host="0.0.0.0", port=80)
     # fastwsgi.run(wsgi_app=app, host='0.0.0.0', port=80)
     # app.run(debug=True, host="0.0.0.0", port=80, threaded=True)
     # app.run(debug=True, host="0.0.0.0", port=80, threaded=False, processes=64)
-
-    from werkzeug.middleware.profiler import ProfilerMiddleware
-    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[10], profile_dir='./profile')
-    app.run(debug=True, host="0.0.0.0", port=80, threaded=True)
